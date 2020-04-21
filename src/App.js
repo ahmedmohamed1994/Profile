c79b753d7b1911947bddbcde83402a9ddb5f9194
AhmedHussein <eng.ahmedmohamed.cs@gmail.com>
2020-04-21 19:30:51 +0300

    Adding structure

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Routes from './Routes';
 class App extends Component {
   render(){
     return (
-      <BrowserRouter >
+      <BrowserRouter basename="/profile/">
       <div className="App">
         <Layout>
        
